da67661401ad8dc2af817ef41cf2931c01f720a8
Danilo Diniz <danilogabriel.48gta@gmail.com>
2026-02-03 21:32:12 -0300

    primeiro exercicio

---
+++ exercicios/src/exercicio01.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+
+public class exercicio01 {
+
+	public static void main(String[] args) {
+		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		byte age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measue = 53.234567;
+		
+		System.out.printf("Products:%n");
+		System.out.printf("%s, wich price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, wich price is $ %.2f%n", product2, price2);
+        
+		System.out.printf("%nRecord: %d years, code %d and gender: %c%n", age, code, gender);
+
+		System.out.printf("%nMeasue with eight decimal places: %.8f%n", measue);
+		System.out.printf("Rouded (three decimal places): %.3f%n", measue);
+		
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f", measue);
+	}
+}
