a1b0fe7e91b294909a92754c35c02a372976a205
Danilo Diniz <danilogabriel.48gta@gmail.com>
2026-02-04 01:48:39 -0300

    finalizando limpeza de interface

--- exercicios/src/ExercicioUm.java
+++ exercicios/src/ExercicioUm.java
@@ -13,18 +13,18 @@ public class ExercicioUm {
 		
 		double price1 = 2100.0;
 		double price2 = 650.50;
-		double measue = 53.234567;
+		double measure = 53.234567;
 		
 		System.out.printf("Products:%n");
-		System.out.printf("%s, wich price is $ %.2f%n", product1, price1);
-		System.out.printf("%s, wich price is $ %.2f%n", product2, price2);
+		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
         
 		System.out.printf("%nRecord: %d years, code %d and gender: %c%n", age, code, gender);
 
-		System.out.printf("%nMeasue with eight decimal places: %.8f%n", measue);
-		System.out.printf("Rouded (three decimal places): %.3f%n", measue);
+		System.out.printf("%nMeasure with eight decimal places: %.8f%n", measure);
+		System.out.printf("Rounded (three decimal places): %.3f%n", measure);
 		
 		Locale.setDefault(Locale.US);
-		System.out.printf("US decimal point: %.3f", measue);
+		System.out.printf("US decimal point: %.3f", measure);
 	}
 }
