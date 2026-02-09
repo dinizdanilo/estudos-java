7529a770ef01ecc59f7ec92ed85bfc19adb5ec8c
Danilo Diniz <dgdevbr@gmail.com>
2026-02-09 20:47:25 -0300

    exercicio condicional quadrante

--- exercicios/src/Quadrante.java
+++ exercicios/src/Quadrante.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Quadrante {
+
+    public static void main(String[] args) {
+
+       Locale.setDefault(Locale.US);
+       Scanner sc = new Scanner(System.in);
+
+       System.out.print("Digite o número do eixo X: ");
+       double x = sc.nextDouble();
+
+       System.out.print("Digite o número do eixo Y: ");
+       double y = sc.nextDouble();
+
+       sc.close();
+
+       if (x > 0 && y > 0) {
+          System.out.printf("\nO eixo de X = %.1f e Y = %.1f está no Quadrante 1", x, y);
+       } else if (x < 0 && y > 0) {
+          System.out.printf("\nO eixo de X = %.1f e Y = %.1f está no Quadrante 2", x, y);
+       } else if (x < 0 && y < 0){
+          System.out.printf("\nO eixo de X = %.1f e Y = %.1f está no Quadrante 3", x, y);
+       } else if (x > 0 && y < 0) {
+          System.out.printf("\nO eixo de X = %.1f e Y = %.1f está no Quadrante 4", x, y);
+       }
+       else {
+          System.out.println("\nO seu eixo está na origem (0)");
+       }
+    }
 }
