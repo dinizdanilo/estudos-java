25ef739018a056728972b7d14495a2e3b1410fce
Danilo Diniz <dgdevbr@gmail.com>
2026-02-09 08:32:34 -0300

    exercicio simples para verificação de número par ou impar

--- exercicios/src/ParOuImpar.java
+++ exercicios/src/ParOuImpar.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ParOuImpar {
+
+    public static void main(String[] args)  {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        sc.close();
+
+        if ((numero % 2) == 0) {
+            System.out.printf("O número que %d é par!", numero);
+        }
+        else {
+            System.out.printf("O número %d é ímpar!", numero);
+        }
+    }
 }
