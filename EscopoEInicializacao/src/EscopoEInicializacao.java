3aab44cf55017f835d8ab6d420f9cbea017d85d9
Danilo Diniz <dgdevbr@gmail.com>
2026-02-10 10:57:54 -0300

    escopo e inicialização

--- EscopoEInicializacao/src/EscopoEInicializacao.java
+++ EscopoEInicializacao/src/EscopoEInicializacao.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
 public class EscopoEInicializacao {
+
+    public static void main (String[] args) {
+
+        double price = 400.00;
+
+
+
+//        if (price < 200) {
+//            double discount = price * 0.1;
+//        }
+//
+//       variavel fora do escopo
+//
+
+        double discount;
+
+        if (price < 200) {
+            discount = price * 0.1;
+        }
+        else {
+            discount = 0;
+        }
+        System.out.println(discount);
+
+//         inicialização
+
+//        erro por não atribuição da variavel
+//        double price;
+//        System.out.println(price);
+
+        System.out.println(price);
+    }
 }
