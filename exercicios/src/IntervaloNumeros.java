b8dee7a6a9db8056d176639924b93c2487ae8ad0
Danilo Diniz <dgdevbr@gmail.com>
2026-02-12 17:47:31 -0300

    exercicio para verificar intervalos utilizando for

--- exercicios/src/IntervaloNumeros.java
+++ exercicios/src/IntervaloNumeros.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class IntervaloNumeros {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite quantos valores você quer conferir: ");
+        int n = sc.nextInt();
+
+        int in = 0;
+        int out = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite um número: ");
+            int x = sc.nextInt();
+            if (x >= 10 && x <= 20) {
+                in ++;
+            }
+            else {
+                out ++;
+            }
+        }
+
+        System.out.println(in + " in");
+        System.out.println(out + " out");
+    }
 }
