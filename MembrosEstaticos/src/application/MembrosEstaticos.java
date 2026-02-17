c9f4cc6f20b4e7d741c3719c3fa21776e210cd31
Danilo Diniz <dgdevbr@gmail.com>
2026-02-17 17:38:18 -0300

    correção na entrada do raio

--- MembrosEstaticos/src/application/MembrosEstaticos.java
+++ MembrosEstaticos/src/application/MembrosEstaticos.java
@@ -14,7 +14,7 @@ public class MembrosEstaticos {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter radius: ");
+        System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
         double c = Calculator.circumference(radius);
