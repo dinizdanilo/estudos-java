025817b31e434dabcf1d71f501c508f88140585a
Danilo Diniz <dgdevbr@gmail.com>
2026-02-25 09:54:15 -0300

    recebendo os metodos get e set para product price

--- OrientacaoObjetosDois/src/application/Main.java
+++ OrientacaoObjetosDois/src/application/Main.java
@@ -24,6 +24,9 @@ public class Main {
         Product product = new Product(name, price);
 
         product.setName("Computer");
+        System.out.println("Updated name: " + product.getName());
+        product.setPrice(1200.00);
+        System.out.println("Updated price: " + product.getPrice());
 
         System.out.println("\nProduct data: " + product);
 
